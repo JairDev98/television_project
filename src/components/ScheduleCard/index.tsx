f9452c0915e0d5d0d3caf6d326a146a9b7fe1218
Jair Aparecido <jair.estu19@gmail.com>
2021-08-17 14:23:53 -0300

    TvTupi2.0_17_08_21

--- src/components/ScheduleCard/index.tsx
+++ src/components/ScheduleCard/index.tsx
@@ -7,6 +7,7 @@ interface Card{
     title:string;
     image:string;
     information:string;
+    url: string;
 }
 
 export function Card(){
@@ -14,42 +15,51 @@ export function Card(){
         {
           title:"Sua Vida me Pertence",
           image:"/images/sua_vida.png",
-          information:"Exibida entre 21 de dezembro de 1951 e 8 de fevereiro de 1952."},
+          information:"Exibida entre 21 de dezembro de 1951 e 8 de fevereiro de 1952.",
+          url:"https://pt.wikipedia.org/wiki/Sua_Vida_Me_Pertence"
+        },
         
         {
           title:"Pollyana",
           image:"/images/pollyana.jpg",
-          information:"Exibida entre 9 de outubro de 1956 e 17 de janeiro de 1957."
+          information:"Exibida entre 9 de outubro de 1956 e 17 de janeiro de 1957.",
+          url:"https://pt.wikipedia.org/wiki/Pollyana_(telenovela)"
         },
 
         {
-          title:"Lever no Espaço",
-          image:"/images/lever.jpg",
-          information:"Exbida entre 12 de janeiro de 1957 e no do mesmo ano."
+          title:"Pollyana Moça",
+          image:"/images/polimoca.jpg",
+          information:"Exbida entre 8 de junho de 1958 e 16 de dezembro de 1958.",
+          url:"https://pt.wikipedia.org/wiki/Pollyana_Mo%C3%A7a_(telenovela)"
         },
+
         {
           title:"A Gata",
           image:"/images/gata.jpg",
-          information:"Exibida entre 11 de maio de 1964 e 2 de julho de 1964."
+          information:"Exibida entre 11 de maio de 1964 e 2 de julho de 1964.",
+          url:"https://pt.wikipedia.org/wiki/A_Gata"
         },
 
         {
           title:"O Direito de Nascer",
           image:"/images/nascer.jpg",
-          information:"Exibida entre 7 de dezembro de 1964 e 13 de agosto de 1965."
+          information:"Exibida entre 7 de dezembro de 1964 e 13 de agosto de 1965.",
+          url:"https://pt.wikipedia.org/wiki/O_Direito_de_Nascer_(1964)"
         },
 
         {
-          title:"Éramos Seis",
-          image:"/images/eramos.JPG",
-          information:"Exibida entre 1 de maio de 1967 e no final de 1967."
-        }]; 
+          title:"Beto Rockfeller",
+          image:"/images/rock.jpg",
+          information:"Exibida entre 4 de novembro de 1968 e 30 de novembro de 1969.",
+          url:"https://pt.wikipedia.org/wiki/Beto_Rockfeller"
+        },]; 
 
     return(
         <>
             <div className={styles.cardContainer}>
                 <a>
                 {cards.map(card=>(
+                  <a href={card.url} className={styles.textTag} target="_blank">
                     <div className={styles.card} key={card.title}>
                         <img src={card.image} alt={card.title} />
                             <div className={styles.information}>
@@ -57,6 +67,7 @@ export function Card(){
                                 <p>{card.information}</p>
                             </div>
                     </div>
+                    </a>
                 ))}    
                 </a>
             </div>
--- src/components/StarsCarousel/index.tsx
+++ src/components/StarsCarousel/index.tsx
@@ -11,23 +11,24 @@ interface istarMenu{
     id: number;
     star: string;
     name:string;
+    url: string;
 }
 
 export function StarMenu(){
     const stars: istarMenu[] = [
-        {id: 1, star:"/images/lima.jpg", name:"LIMA DUARTE"},
-        {id: 2, star:"/images/hebe.jpg", name:"HEBE CAMARGO"},
-        {id: 3, star:"/images/lolita.jpg", name:"LOLITA RODRIGUES"},
-        {id: 4, star:"/images/assis.jpg", name:"ASSIS CHATEAUBRIAND"},
-        {id: 5, star:"/images/inezita.jpg", name:"INEZITA BARROSO"},
-        {id: 6, star:"/images/eva.jpg", name:"EVA WILMA"},
-        {id: 7, star:"/images/guarnieri.png", name:"GIANFRANCESCO GUARNIERI"},
-        {id: 8, star:"/images/silvio.jpg", name:"SILVIO SANTOS"},
-        {id: 9, star:"/images/dermival.jpg", name:"DERMIVAL COSTA LIMA"},
-        {id: 10, star:"/images/cassiano.jpg", name:"CASSIANO GABUS MENDES"},
-        {id: 11, star:"/images/gloria.png", name:"GLORIA MENEZES"},
-        {id: 12, star:"/images/tarcisio.jpg", name:"TARCISIO MEIRA"},
-        {id: 13, star:"/images/tony.png", name:"TONY RAMOS"},
+        {id: 1, star:"/images/lima.jpg", url:"https://pt.wikipedia.org/wiki/Lima_Duarte", name:"LIMA DUARTE"},
+        {id: 2, star:"/images/hebe.jpg", url:"https://pt.wikipedia.org/wiki/Hebe_Camargo",name:"HEBE CAMARGO"},
+        {id: 3, star:"/images/lolita.jpg", url:"https://pt.wikipedia.org/wiki/Lolita_Rodrigues",name:"LOLITA RODRIGUES"},
+        {id: 4, star:"/images/assis.jpg", url:"https://pt.wikipedia.org/wiki/Assis_Chateaubriand",name:"ASSIS CHATEAUBRIAND"},
+        {id: 5, star:"/images/inezita.jpg", url:"https://pt.wikipedia.org/wiki/Inezita_Barroso",name:"INEZITA BARROSO"},
+        {id: 6, star:"/images/eva.jpg", url:"https://pt.wikipedia.org/wiki/Eva_Wilma",name:"EVA WILMA"},
+        {id: 7, star:"/images/guarnieri.png", url:"https://pt.wikipedia.org/wiki/Gianfrancesco_Guarnieri",name:"GIANFRANCESCO GUARNIERI"},
+        {id: 8, star:"/images/silvio.jpg", url:"https://pt.wikipedia.org/wiki/Silvio_Santos",name:"SILVIO SANTOS"},
+        {id: 9, star:"/images/dermival.jpg", url:"https://www.museudatv.com.br/biografia/dermival-costa-lima/",name:"DERMIVAL COSTA LIMA"},
+        {id: 10, star:"/images/cassiano.jpg", url:"https://pt.wikipedia.org/wiki/Cassiano_Gabus_Mendes",name:"CASSIANO GABUS MENDES"},
+        {id: 11, star:"/images/nicette.png", url:"https://pt.wikipedia.org/wiki/Nicette_Bruno",name:"NICETTE BRUNO"},
+        {id: 12, star:"/images/tarcisio.jpg", url:"https://pt.wikipedia.org/wiki/Tarc%C3%ADsio_Meira",name:"TARCISIO MEIRA"},
+        {id: 13, star:"/images/tony.png", url:"https://pt.wikipedia.org/wiki/Tony_Ramos",name:"TONY RAMOS"},
     ];
     
     const settings = {
@@ -73,8 +74,10 @@ export function StarMenu(){
              {stars.map(star=>(
                  <nav className={styles.starContainer}>
             <div key={star.id}>
-                <img src={star.star} alt="Logo" />
-                <p>{star.name}</p>
+                <a href={star.url} target="_blank">
+                    <img src={star.star} alt="Logo" />
+                    <p>{star.name}</p>
+                </a>
             </div>
                 </nav>
                 ))}    
