1351cce860556f04411fdb6782c46e205e4e3097
Jair Aparecido de Oliveira <jair.estu19@gmail.com>
2021-09-07 21:59:13 -0300

    07_09_21

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document{
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"/>
                 <link rel="shortcut icon" href="icons/favicon.ico"/>
+                <title>TV Tupi</title>
                 </Head>
                 <body>
                     <Main />
